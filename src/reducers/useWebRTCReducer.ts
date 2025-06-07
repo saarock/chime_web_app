43288b53f3f6316de34b5ac997da1c7b317e3e27
saarock <saarock4646@gmail.com>
2025-06-07 12:40:20 +0545

    changes webRTC useStates to useReducer state management system

--- src/reducers/useWebRTCReducer.ts
+++ src/reducers/useWebRTCReducer.ts
@@ -1,5 +1,5 @@
 // Types
-type State = {
+export type State = {
     localStream: MediaStream | null;
     remoteStream: MediaStream | null;
     errorMessage: string | null;
@@ -9,7 +9,14 @@ type State = {
     isVideoSocketConnected: boolean;
 }
 
-type Action =
+interface VideoConnectionStatusPayload {
+    isVideoSocketConnected: boolean;
+    errorMessage: string | null;
+    onlineUsersCount: number;
+}
+
+
+export type Action =
     | { type: 'SET_LOCAL_STREAM'; payload: MediaStream | null }
     | { type: 'SET_REMOTE_STREAM'; payload: MediaStream | null }
     | { type: 'SET_ERROR_MESSAGE'; payload: string | null }
@@ -17,9 +24,12 @@ type Action =
     | { type: 'SET_ONLINE_USERS_COUNT'; payload: number }
     | { type: 'SET_PARTNER_CALL_ENDED'; payload: boolean }
     | { type: 'SET_VIDEO_SOCKET_CONNECTED'; payload: boolean }
+    | { type: "SET_REMOTE_STREAM_NULL_AND_UPDATE_SUCCESS_MESSAGE"; payload: string }
+    | { type: "UPDATE_VIDEO_CONNECTED_ERROR_MESSAGE_AND_ONLINE_USER", payload: VideoConnectionStatusPayload}
+    | {type: "RESET_ERROR_AND_SUCCESS_MESSAGE"}
 
 // Initial State
-const initialState: State = {
+export const initialState: State = {
     localStream: null,
     remoteStream: null,
     errorMessage: null,
@@ -30,7 +40,7 @@ const initialState: State = {
 }
 
 // Reducer Function
-const reducer = (state: State, action: Action): State => {
+export const webRTCReducer = (state: State, action: Action): State => {
     switch (action.type) {
         case 'SET_LOCAL_STREAM':
             return { ...state, localStream: action.payload }
@@ -46,6 +56,13 @@ const reducer = (state: State, action: Action): State => {
             return { ...state, isPartnerCallEnded: action.payload }
         case 'SET_VIDEO_SOCKET_CONNECTED':
             return { ...state, isVideoSocketConnected: action.payload }
+        case "SET_REMOTE_STREAM_NULL_AND_UPDATE_SUCCESS_MESSAGE":
+            return { ...state, remoteStream: null, successMessage: action.payload }
+        case "UPDATE_VIDEO_CONNECTED_ERROR_MESSAGE_AND_ONLINE_USER":
+            return {...state, isVideoSocketConnected: action.payload.isVideoSocketConnected, errorMessage: action.payload.errorMessage, onlineUsersCount: action.payload.onlineUsersCount}
+        case "RESET_ERROR_AND_SUCCESS_MESSAGE":
+            return {...state, errorMessage: null, successMessage: null}
+
         default:
             return state
     }
